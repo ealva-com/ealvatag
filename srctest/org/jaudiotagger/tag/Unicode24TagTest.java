976bd6f380fdb44da0304f1ef95747806d6ad7fa
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2006-06-11 20:35:04 +0000

    Issue #34:Unable to read COMM frames correctly that are UTF8 encoded:test

---
+++ srctest/org/jaudiotagger/tag/Unicode24TagTest.java
@@ -0,0 +1,48 @@
+package org.jaudiotagger.tag;
+
+import java.io.IOException;
+
+import junit.textui.TestRunner;
+
+import org.jaudiotagger.AbstractTestCase;
+import org.jaudiotagger.audio.InvalidAudioFrameException;
+import org.jaudiotagger.audio.ReadOnlyFileException;
+import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.id3.*;
+import org.jaudiotagger.tag.id3.framebody.FrameBodyCOMM;
+
+public class Unicode24TagTest extends AbstractTestCase {
+
+    public void testUTF8WithNullTerminator () throws IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException, RuntimeException {
+        MP3File mp3File
+            = new MP3File (copyAudioToTmp("testV24-comments-utf8.mp3"));
+        AbstractID3v2Tag id3v2Tag = mp3File.getID3v2Tag();
+        assertNotNull (id3v2Tag);
+        AbstractID3v2Frame frame
+        = (AbstractID3v2Frame) id3v2Tag.getFrame("COMM");
+        assertNotNull (frame);
+        AbstractTagFrameBody frameBody = frame.getBody();
+        assertTrue (frameBody instanceof FrameBodyCOMM);
+        FrameBodyCOMM commFrameBody = (FrameBodyCOMM) frameBody;
+        //String borodin = "\u0411\u043e\u0440\u043e\u0434\u0438\u043d";
+        byte UTF8_ENCODING = (byte) 3;
+        String language = "eng";
+        String comment = "some comment here";
+        String description = "cc";
+        FrameBodyCOMM targetFrameBody = new FrameBodyCOMM (UTF8_ENCODING, language, description, comment);
+        ID3v24Frame newFrame = new ID3v24Frame(targetFrameBody);
+        assertEquals (UTF8_ENCODING,targetFrameBody.getTextEncoding() );
+        assertEquals (language,targetFrameBody.getLanguage ());
+        assertEquals (description,targetFrameBody.getDescription () );
+        assertEquals (comment,targetFrameBody.getText());
+        assertEquals (UTF8_ENCODING,commFrameBody.getTextEncoding());
+        assertEquals (language,commFrameBody.getLanguage ());
+        assertEquals (description,commFrameBody.getDescription () );
+        assertEquals (comment,commFrameBody.getText () );
+        assertEquals (targetFrameBody,commFrameBody );
+    }
+
+    public static void main (String [] args) throws Exception {
+        TestRunner.run(Unicode24TagTest.class);
+    }
+}
