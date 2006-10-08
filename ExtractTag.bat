ccbde39347f07465a1333419e74c815874d01d20
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2006-10-08 10:55:46 +0000

    Issue #7:Added support for extracting a tag out of an mp3 file

---
+++ ExtractTag.bat
@@ -0,0 +1,2 @@
+set classpath=%classpath%;.\classes;\dist\jaudiotagger.jar
+java org.jaudiotagger.test.ExtractID3TagFromFile %1 %2
---
+++ src/org/jaudiotagger/test/ExtractID3TagFromFile.java
@@ -0,0 +1,50 @@
+package org.jaudiotagger.test;
+
+import org.jaudiotagger.audio.mp3.MP3File;
+
+import java.io.File;
+import java.util.Date;
+import java.text.DateFormat;
+
+/**
+ * Simple class that will attempt to recusively read all files within a directory, flags
+ * errors that occur.
+ */
+public class ExtractID3TagFromFile
+{
+
+    public static void main(final String[] args)
+    {
+        ExtractID3TagFromFile test = new ExtractID3TagFromFile();
+
+        if(args.length!=2)
+        {
+            System.err.println("usage ExtractID3TagFromFile Filename FilenameOut");
+            System.err.println("      You must enter the file to extract the tag from and where to extract to");
+            System.exit(1);
+        }
+
+        File file    = new File(args[0]);
+        File outFile = new File(args[1]);
+        if(!file.isFile())
+        {
+            System.err.println("usage ExtractID3TagFromFile Filename FilenameOut");
+            System.err.println("      File "+args[0] +" could not be found");
+            System.exit(1);
+        }
+
+         try
+         {
+            final MP3File tmpMP3 = new MP3File(file);
+            tmpMP3.extractID3v2TagDataIntoFile(outFile);
+         }
+         catch(Exception e)
+         {
+              System.err.println("Unable to extract tag");
+              System.exit(1);
+         }
+    }
+
+    public static final String IDENT = "$Id$";
+
+}
