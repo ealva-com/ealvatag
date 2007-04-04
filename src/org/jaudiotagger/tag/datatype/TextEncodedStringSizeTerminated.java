d7d208733847933774a123d6f14dd4eee3e95f7a
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2007-04-04 09:20:07 +0000

    Issue #94:TextEncodedString Decoder doesn't handle errors well, with this fix if there is a decoding problem half way through decoding a string the successfully decoded portion will now be kept. Also decoding was against the whole framebody instead of just against the text string because we were wrapping but not slicing the buffer

--- src/org/jaudiotagger/tag/datatype/TextEncodedStringSizeTerminated.java
+++ src/org/jaudiotagger/tag/datatype/TextEncodedStringSizeTerminated.java
@@ -9,10 +9,10 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 
 /**
- * Represents a String which is not delimited by null character. 
- * 
- * This type of String will usually only be used when it is the last field within a frame, when reading the remainder of the byte array will
- * be read, when writing the frame will be accomodate the required size for the String. The String
+ * Represents a String which is not delimited by null character.
+ * <p/>
+ * This type of String will usually only be used when it is the last field within a frame, when reading the remainder of
+ * the byte array will be read, when writing the frame will be accomodate the required size for the String. The String
  * will be encoded based upon the text encoding of the frame that it belongs to.
  */
 public class TextEncodedStringSizeTerminated
@@ -22,7 +22,7 @@ public class TextEncodedStringSizeTerminated
     /**
      * Creates a new ObjectStringSizeTerminated datatype.
      *
-     * @param identifier  identifies the frame type
+     * @param identifier identifies the frame type
      */
     public TextEncodedStringSizeTerminated(String identifier, AbstractTagFrameBody frameBody)
     {
@@ -47,47 +47,48 @@ public class TextEncodedStringSizeTerminated
      * Read a 'n' bytes from buffer into a string where n is the framesize - offset
      * so thefore cannot use this if there are other objects after it because it has no
      * delimiter.
-     *
+     * <p/>
      * Must take into account the text encoding defined in the Encoding Object
      * ID3 Text Frames often allow multiple strings seperated by the null char
      * appropriate for the encoding.
      *
      * @param arr    this is the buffer for the frame
      * @param offset this is where to start reading in the buffer for this field
-     * @throws NullPointerException      
-     * @throws IndexOutOfBoundsException 
+     * @throws NullPointerException
+     * @throws IndexOutOfBoundsException
      */
     public void readByteArray(byte[] arr, int offset) throws InvalidDataTypeException
     {
-        try
+        logger.finest("Reading from array from offset:" + offset);
+
+        //Get the Specified Decoder
+        String charSetName = getTextEncodingCharSet();
+        CharsetDecoder decoder = Charset.forName(charSetName).newDecoder();
+
+        //Decode sliced inBuffer
+        ByteBuffer inBuffer = ByteBuffer.wrap(arr, offset, arr.length - offset).slice();
+        CharBuffer outBuffer = CharBuffer.allocate(arr.length - offset);
+        decoder.reset();
+        CoderResult coderResult = decoder.decode(inBuffer, outBuffer, true);
+        if (coderResult.isError())
         {
-            logger.finest("Reading from array from offset:" + offset);
-            //Get the Specified Decoder
-            String charSetName = getTextEncodingCharSet();
-            CharsetDecoder decoder = Charset.forName(charSetName).newDecoder();
-            //Decode buffer if runs into problems should throw exception which we
-            //catch and then set value to empty string.
-            String str = decoder.decode(ByteBuffer.wrap(arr, offset, arr.length - offset)).toString();
-            if (str == null)
-            {
-                throw new NullPointerException("String is null");
-            }
-            value = str;
+            logger.warning("Decoding error:" + coderResult.toString());
         }
-        catch (CharacterCodingException ce)
-        {
-            logger.severe(ce.getMessage());
-            value = "";
-        }
+        decoder.flush(outBuffer);
+        outBuffer.flip();
+        value = outBuffer.toString();
+
+        //IF SIZE NOT WHAT WAS EXPECTED , CHECK FOR NULL TERMINATORS ?
+        //TODO Size
         setSize(arr.length - offset);
-        logger.info("Read SizeTerminatedString:" + value+" size:"+size);          
+        logger.info("Read SizeTerminatedString:" + value + " size:" + size);
     }
 
     /**
      * Write String into byte array
      *
      * @return the dat as a byte array in format to write to file
-    */
+     */
     public byte[] writeByteArray()
     {
         byte[] data = null;
@@ -110,11 +111,11 @@ public class TextEncodedStringSizeTerminated
         return data;
     }
 
-    protected String  getTextEncodingCharSet()
+    protected String getTextEncodingCharSet()
     {
-         byte textEncoding = this.getBody().getTextEncoding();
-         String charSetName = TextEncoding.getInstanceOf().getValueForId(textEncoding);
-         logger.finest("text encoding:"+textEncoding + " charset:"+charSetName);
+        byte textEncoding = this.getBody().getTextEncoding();
+        String charSetName = TextEncoding.getInstanceOf().getValueForId(textEncoding);
+        logger.finest("text encoding:" + textEncoding + " charset:" + charSetName);
         return charSetName;
     }
 }
