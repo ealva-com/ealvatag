bbf2d5c4c78876718b8e74352c1b3c78b60134ca
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2007-11-21 16:46:45 +0000

    Issue #129:Flac support improved tests

--- srctest/org/jaudiotagger/audio/flac/FlacHeaderTest.java
+++ srctest/org/jaudiotagger/audio/flac/FlacHeaderTest.java
@@ -3,6 +3,7 @@ package org.jaudiotagger.audio.flac;
 import org.jaudiotagger.AbstractTestCase;
 import org.jaudiotagger.tag.vorbiscomment.VorbisCommentTag;
 import org.jaudiotagger.tag.vorbiscomment.VorbisCommentFieldKey;
+import org.jaudiotagger.tag.TagFieldKey;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 
@@ -23,19 +24,31 @@ public class FlacHeaderTest extends TestCase
             File testFile = AbstractTestCase.copyAudioToTmp("test.flac");
             AudioFile f = AudioFileIO.read(testFile);
 
-            assertEquals("744",f.getAudioHeader().getBitRate());
+            assertEquals("206",f.getAudioHeader().getBitRate());
             assertEquals("FLAC 16 bits",f.getAudioHeader().getEncodingType());
             assertEquals("2",f.getAudioHeader().getChannels());
             assertEquals("44100",f.getAudioHeader().getSampleRate());
 
 
             assertTrue(f.getTag() instanceof VorbisCommentTag);
-            assertEquals("02",f.getTag().getFirstTrack());
-            assertEquals("-1.22 dB",f.getTag().getFirst("REPLAYGAIN_TRACK_GAIN"));
-            assertEquals("sssss",f.getTag().getFirst("ARTIST"));
-            assertEquals("0.99996948",f.getTag().getFirst("REPLAYGAIN_TRACK_PEAK"));
-            assertEquals("-1.22 dB",f.getTag().getFirst("REPLAYGAIN_ALBUM_GAIN"));
-            assertEquals("0.99996948",f.getTag().getFirst("REPLAYGAIN_ALBUM_PEAK"));
+            VorbisCommentTag tag = (VorbisCommentTag)f.getTag();
+              //Ease of use methods for common fields
+            assertEquals("Artist", tag.getFirstArtist());
+            assertEquals("Album", tag.getFirstAlbum());
+            assertEquals("test3", tag.getFirstTitle());
+            assertEquals("comments", tag.getFirstComment());
+            assertEquals("1971", tag.getFirstYear());
+            assertEquals("4", tag.getFirstTrack());
+            assertEquals("Crossover", tag.getFirstGenre());
+
+            //Lookup by generickey
+            assertEquals("Artist", tag.getFirst(TagFieldKey.ARTIST));
+            assertEquals("Album", tag.getFirst(TagFieldKey.ALBUM));
+            assertEquals("test3", tag.getFirst(TagFieldKey.TITLE));
+            assertEquals("comments", tag.getFirst(TagFieldKey.COMMENT));
+            assertEquals("1971", tag.getFirst(TagFieldKey.YEAR));
+            assertEquals("4", tag.getFirst(TagFieldKey.TRACK));
+            assertEquals("Composer", tag.getFirst(TagFieldKey.COMPOSER));           
         }
         catch(Exception e)
         {
@@ -44,4 +57,28 @@ public class FlacHeaderTest extends TestCase
         }
         assertNull(exceptionCaught);
     }
+
+     public void testReadFileWithNoVorbisComment()
+    {
+        Exception exceptionCaught = null;
+        try
+        {
+            File testFile = AbstractTestCase.copyAudioToTmp("test2.flac");
+            AudioFile f = AudioFileIO.read(testFile);
+
+            assertEquals("206",f.getAudioHeader().getBitRate());
+            assertEquals("FLAC 16 bits",f.getAudioHeader().getEncodingType());
+            assertEquals("2",f.getAudioHeader().getChannels());
+            assertEquals("44100",f.getAudioHeader().getSampleRate());
+
+            assertTrue(f.getTag() instanceof VorbisCommentTag);
+        }
+        catch(Exception e)
+        {
+             e.printStackTrace();
+             exceptionCaught = e;
+        }
+        assertNull(exceptionCaught);
+    }
+
 }
