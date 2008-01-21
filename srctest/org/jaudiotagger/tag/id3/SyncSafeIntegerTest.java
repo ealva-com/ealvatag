cdbf0e63617067be1068c63d3a2de9ac7eff36f8
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2008-01-21 21:56:35 +0000

    Case problem

--- srctest/org/jaudiotagger/tag/id3/SyncSafeIntegerTest.java
+++ srctest/org/jaudiotagger/tag/id3/SyncSafeIntegerTest.java
@@ -17,7 +17,7 @@ public class SyncSafeIntegerTest extends AbstractTestCase
         Exception e = null;
         try
         {
-            File testFile = AbstractTestCase.copyAudioToTmp("issue158.id3","testv1.mp3");
+            File testFile = AbstractTestCase.copyAudioToTmp("issue158.id3","testV1.mp3");
             MP3File mp3File = new MP3File(testFile);
             //Read frame that contains the byte>128 value
             assertTrue(mp3File.getID3v2Tag().hasFrame("USLT"));
