a0ddf91028ab8ea89fdbb2d1b07faa5d2cd84e21
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2008-02-27 11:21:32 +0000

    Missing File

---
+++ src/org/jaudiotagger/logging/Hex.java
@@ -0,0 +1,18 @@
+package org.jaudiotagger.logging;
+
+/**
+ * Display as hex
+ */
+public class Hex
+{
+    /**
+     * Display as hex
+     * 
+     * @param value
+     * @return
+     */
+    public static String asHex(long value)
+    {
+        return "0x" + Long.toHexString(value);
+    }
+}
