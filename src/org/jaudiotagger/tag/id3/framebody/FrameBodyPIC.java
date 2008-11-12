ac30022fb6f1555d29f498fd55ade03302b19593
paultaylor <paultaylor@922cec90-98bc-ecba-b0a9-867c771b4a74>
2008-11-12 19:39:13 +0000

    Improved comment

--- src/org/jaudiotagger/tag/id3/framebody/FrameBodyPIC.java
+++ src/org/jaudiotagger/tag/id3/framebody/FrameBodyPIC.java
@@ -48,6 +48,37 @@ import java.nio.ByteBuffer;
  * format' "-->" and having a complete URL [URL] instead of picture data.
  * The use of linked files should however be used restrictively since
  * there is the risk of separation of files.
+ * <p/>
+ * Attached picture   "PIC"
+ * Frame size         $xx xx xx
+ * Text encoding      $xx
+ * Image format       $xx xx xx
+ * Picture type       $xx
+ * Description        <textstring> $00 (00)
+ * Picture data       <binary data>
+ * <p/>
+ * <p/>
+ * Picture type:  $00  Other
+ * $01  32x32 pixels 'file icon' (PNG only)
+ * $02  Other file icon
+ * $03  Cover (front)
+ * $04  Cover (back)
+ * $05  Leaflet page
+ * $06  Media (e.g. lable side of CD)
+ * $07  Lead artist/lead performer/soloist
+ * $08  Artist/performer
+ * $09  Conductor
+ * $0A  Band/Orchestra
+ * $0B  Composer
+ * $0C  Lyricist/text writer
+ * $0D  Recording Location
+ * $0E  During recording
+ * $0F  During performance
+ * $10  Movie/video screen capture
+ * $11  A bright coloured fish
+ * $12  Illustration
+ * $13  Band/artist logotype
+ * $14  Publisher/Studio logotype
  */
 public class FrameBodyPIC extends AbstractID3v2FrameBody implements ID3v22FrameBody
 {
