bc6d3148d017fbe757640fbd7dc7523269615917
ijabz <ijabz@fastmail.fm>
2015-10-06 11:39:16 +0100

    Issue #49: (was #433) extra tests to ensure can cope with udta atom with unkown atoms plus meta atom

--- srctest/org/jaudiotagger/issues/Issue433Test.java
+++ srctest/org/jaudiotagger/issues/Issue433Test.java
@@ -66,4 +66,55 @@ public class Issue433Test extends AbstractTestCase
         assertEquals("fred",af.getTag().getFirst(FieldKey.ALBUM));
     }
 
+    public void testWriteMp4LargeIncreaseExistingUdtaWithMetaDataAndUnknownAddDataLarge() throws Exception
+    {
+        Exception ex=null;
+        File orig = new File("testdata", "test141.m4a");
+        if (!orig.isFile())
+        {
+            System.err.println("Unable to test file - not available");
+            return;
+        }
+
+        File testFile = AbstractTestCase.copyAudioToTmp("test141.m4a",new File("test141Large.m4a"));
+
+        Mp4AtomTree atomTree = new Mp4AtomTree(new RandomAccessFile(testFile, "r"));
+        atomTree.printAtomTree();
+
+        AudioFile af = AudioFileIO.read(testFile);
+
+        af.getTag().setField(FieldKey.ALBUM,"fredwwwwwwwwwwwwwwwwwwwwwwww");
+        af.commit();
+
+        atomTree = new Mp4AtomTree(new RandomAccessFile(testFile, "r"));
+        atomTree.printAtomTree();
+        af = AudioFileIO.read(testFile);
+        assertEquals("fredwwwwwwwwwwwwwwwwwwwwwwww",af.getTag().getFirst(FieldKey.ALBUM));
+    }
+
+    public void testWriteMp4LargeIncreaseExistingUdtaWithMetaDataAndUnknownAddDataSmall() throws Exception
+    {
+        Exception ex=null;
+        File orig = new File("testdata", "test141.m4a");
+        if (!orig.isFile())
+        {
+            System.err.println("Unable to test file - not available");
+            return;
+        }
+
+        File testFile = AbstractTestCase.copyAudioToTmp("test141.m4a",new File("test141Small.m4a"));
+
+        Mp4AtomTree atomTree = new Mp4AtomTree(new RandomAccessFile(testFile, "r"));
+        atomTree.printAtomTree();
+
+        AudioFile af = AudioFileIO.read(testFile);
+
+        af.getTag().setField(FieldKey.ALBUM,"fred");
+        af.commit();
+
+        atomTree = new Mp4AtomTree(new RandomAccessFile(testFile, "r"));
+        atomTree.printAtomTree();
+        af = AudioFileIO.read(testFile);
+        assertEquals("fred",af.getTag().getFirst(FieldKey.ALBUM));
+    }
 }
