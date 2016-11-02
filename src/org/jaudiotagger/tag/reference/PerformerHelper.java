9e6e08071fb28ed83ba5a0f251ec6b0fcd50713d
ijabz <ijabz@fastmail.fm>
2016-11-02 14:42:31 +0000

    Always add role as lowercase.

--- src/org/jaudiotagger/tag/reference/PerformerHelper.java
+++ src/org/jaudiotagger/tag/reference/PerformerHelper.java
@@ -2,17 +2,17 @@ package org.jaudiotagger.tag.reference;
 
 /**
  * The PERFORMER field is formatted differently depending on whether writing to the ID3 format that utilises multiple fields or the simple
- * TEXT/VALUE fields used by VorbisComments and similar formats
+ * TEXT/VALUE fields used by VorbisComments and similar formats, the role is always set to lowercase.
  */
 public class PerformerHelper
 {
     public static String formatForId3(String artist, String attributes)
     {
-        return attributes + '\0' + artist;
+        return attributes.toLowerCase() + '\0' + artist;
     }
 
     public static String formatForNonId3(String artist, String attributes)
     {
-        return artist + " (" + attributes + ")";
+        return artist + " (" + attributes.toLowerCase() + ")";
     }
 }
