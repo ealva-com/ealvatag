3ed60ff52d2637a561de33036a44ae60ad45af02
ijabz <ijabz@fastmail.fm>
2016-11-10 17:06:25 +0000

    Mp4 MovementNo/Total Fields

--- srctest/org/jaudiotagger/tag/mp4/ReadMp4MovementTagsTest.java
+++ srctest/org/jaudiotagger/tag/mp4/ReadMp4MovementTagsTest.java
@@ -27,16 +27,20 @@ public class ReadMp4MovementTagsTest extends AbstractTestCase
             File testFile = AbstractTestCase.copyAudioToTmp("test161.m4a");
             AudioFile af = AudioFileIO.read(testFile);
             assertNotNull(af.getTag());
+            assertEquals("I. Preludium (Pastorale). Allegro moderato",af.getTag().getFirst(FieldKey.MOVEMENT));
             assertEquals("2",af.getTag().getFirst(FieldKey.MOVEMENT_NO));
-            assertEquals("2",af.getTag().getFirst(FieldKey.MOVEMENT_TOTAL));
+            assertEquals("0",af.getTag().getFirst(FieldKey.MOVEMENT_TOTAL));
 
+            af.getTag().setField(FieldKey.MOVEMENT, "fred");
             af.getTag().setField(FieldKey.MOVEMENT_NO, "1");
             af.getTag().setField(FieldKey.MOVEMENT_TOTAL, "7");
 
+            assertEquals("fred",af.getTag().getFirst(FieldKey.MOVEMENT));
             assertEquals("1",af.getTag().getFirst(FieldKey.MOVEMENT_NO));
             assertEquals("7",af.getTag().getFirst(FieldKey.MOVEMENT_TOTAL));
             af.commit();
             af = AudioFileIO.read(testFile);
+            assertEquals("fred",af.getTag().getFirst(FieldKey.MOVEMENT));
             assertEquals("1",af.getTag().getFirst(FieldKey.MOVEMENT_NO));
             assertEquals("7",af.getTag().getFirst(FieldKey.MOVEMENT_TOTAL));
 
